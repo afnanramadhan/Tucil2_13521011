fa676eb940071fcf7d427710b58e3b1fffad2669
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-02-25 00:08:39 +0700

    tambah fungsi visual & edit main

--- fungsii.py
+++ fungsii.py
@@ -22,6 +22,27 @@ def printList(list):
         for j in range(len(list[i])):
             print(list[i][j], end=" ")
         print("")
+        
+def show2dAll(list):
+    for i in range(len(list)):
+        x = list[i][0]
+        y = list[i][1]
+        plt.scatter(x, y, color='blue')
+    
+def showNearestDots2D(list,i,j):
+    x1 = list[i][0]
+    x2 = list[j][0]
+    y1 = list[i][1]
+    y2 = list[j][1]
+    plt.scatter(x1, y1, color='red')
+    plt.scatter(x2, y2, color='red')
+    
+def drawLine2D(list,i,j):
+    x = np.array([list[i][0], list[j][0]])
+    y = np.array([list[i][1], list[j][1]])
+    plt.plot(x, y, color='red')
+    
+    
 
 def show3dAll(list,ax):
     for i in range(len(list)):
@@ -30,7 +51,7 @@ def show3dAll(list,ax):
         zs = list[i][2]
         ax.scatter(xs, ys, zs, color='blue')
        
-def showNearestDots(list,i,j,ax):
+def showNearestDots3D(list,i,j,ax):
     xs = list[i][0]
     ys = list[i][1]
     zs = list[i][2]
@@ -41,7 +62,7 @@ def showNearestDots(list,i,j,ax):
     ax.scatter(xx, yy, zz, color='red')
         
         
-def drawLine(list,i,j,ax):
+def drawLine3D(list,i,j,ax):
     x = np.array([list[i][0], list[j][0]])
     y = np.array([list[i][1], list[j][1]])
     z = np.array([list[i][2], list[j][2]])
@@ -49,13 +70,10 @@ def drawLine(list,i,j,ax):
     
 def hitungJarak(list,i,j):
     #i dan j indeks di list
-    x1 = list[i][0]
-    x2 = list[j][0]
-    y1 = list[i][1]
-    y2 = list[j][1]
-    z1 = list[i][2]
-    z2 = list[j][2]
-    jarak = math.sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
+    sum = 0
+    for k in range(len(list[0])):
+        sum+= (list[i][k]-list[j][k])**2
+    jarak = math.sqrt(sum)
     return jarak
 
 def findMinBruteForce(list):
@@ -77,65 +95,68 @@ def splitList(list):
     else:
         return list[:n],list[n+1:]
 
-def findMinDivideConquer(list,n):
-    #n paling
-    if(len(list)==n):
+def findMinDivideConquer(list):
+    #pembagian terkecil sisa 2 titik
+    if(len(list)==2):
         return hitungJarak(list,0,1)
-    elif(len(list)==n+1):
+    elif(len(list)==3):
         return findMinBruteForce(list)[0]
     else:
         l1,l2 = splitList(list)
-        d1 = findMinDivideConquer(l1,n)
-        d2 = findMinDivideConquer(l2,n)
+        d1 = findMinDivideConquer(l1)
+        d2 = findMinDivideConquer(l2)
         d = min(d1,d2)
         
+        tengah = len(list)//2
+        if(len(list)%2==0):
+            avg = (list[tengah][0]+list[tengah+1][0])/2
+        else:
+            avg = (list[tengah-1][0]+list[tengah+1][0])/2
         #cek apakah ada titik yang ada di daerah -d dan +d
         #sekalian dimasukkin ke list temporary
         temp = []
         for i in list:
-            if(i[0]>= len(list)/2-d and i[0]<= len(list)/2+d):
+            if(i[0]>= avg-d and i[0]<= avg+d):
                 temp.append(i)
                 
         #cari minimum jarak yang ada di daerah -d dan +d
         for i in range(len(temp)):
             for j in range(i+1,len(temp)):
-                if(abs(temp[i][0]-temp[j][0])>d or abs(temp[i][1]-temp[j][1])>d or abs(temp[i][2]-temp[j][2])>d):
-                    continue
-                else:
+                for k in range(1,len(temp[0])):
+                    ##abs(temp[i][0]-temp[j][0])>d or abs(temp[i][1]-temp[j][1])>d or abs(temp[i][2]-temp[j][2])>
+                    if(temp[i][k]-temp[j][k]>d):
+                        flag = False
+                        break
+                    else:
+                        flag = True
+                if(flag):
                     d3 = hitungJarak(temp,i,j)
                     if(d3<d):
                         d = d3                 
         return d
     
-def jalan():
-    print("Masukkan jumlah titik: ")
-    n = int(input())
-
-    lll = createDots(n,3)
-    printList(lll)
-    print("")
-    lll.sort()
-    printList(lll)
-    print("")
-
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-
-    print("-----------------------")
-    min,titik1,titik2 = findMinBruteForce(lll)
-    print("-----------------------")
-    print("Jarak terdekat: ")
-    print(min)
-    print(titik1)
-    print(titik2)
-
-    show3dAll(lll,ax)
-    showNearestDots(lll,titik1,titik2,ax)
-    drawLine(lll,titik1,titik2,ax)
-
-    ax.set_xlabel('Sumbu X')
-    ax.set_ylabel('Sumbu Y')
-    ax.set_zlabel('Sumbu Z')
-    plt.show()
-    
-
+def showVisual(dim,list,titik1,titik2,distance):
+    #d dimensi
+    #titik1 dan titik2 indeks di list
+    if(dim==2):
+        print("2D")
+        show2dAll(list)
+        showNearestDots2D(list,titik1,titik2)
+        drawLine2D(list,titik1,titik2)
+        plt.annotate(round(distance), xy=((list[titik1][0]+list[titik2][0])/2, (list[titik1][1]+list[titik2][1])/2))
+        plt.xlabel("Sumbu X")
+        plt.ylabel("Sumbu Y")
+        plt.show()
+        
+    elif(dim==3):
+        print("3D")
+        ax = plt.axes(projection='3d')
+        show3dAll(list,ax)
+        showNearestDots3D(list,titik1,titik2,ax)
+        drawLine3D(list,titik1,titik2,ax)
+        ax.set_xlabel('Sumbu X')
+        ax.set_ylabel('Sumbu Y')
+        ax.set_zlabel('Sumbu Z')
+        plt.show()
+    else:
+        print("MAAF TIDAK DAPAT DITAMPILKAN")
--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 from fungsii import *
 import time
 
-# jalan()
-print("Masukkan jumlah titik: ")
-n = int(input())
-# # print("Masukkan dimensi: ")
-# # d = int(input())
+def main():
+    print("Masukkan jumlah titik: ")
+    n = int(input())
+    print("Masukkan dimensi: ")
+    d = int(input())
 
-lll = createDots(n,3)
-lll.sort()
-# printList(lll)
-# print("")
+    lll = createDots(n,d)
+    lll.sort()
 
-startb = time.time()
-a = findMinBruteForce(lll)[0]
-endb = time.time()
+    startb = time.time()
+    a,titik1,titik2 = findMinBruteForce(lll)
+    endb = time.time()
 
-startd = time.time()
-b = (findMinDivideConquer(lll,2))
-endd = time.time()
+    startd = time.time()
+    b = (findMinDivideConquer(lll,2))
+    endd = time.time()
 
+    
 
-print("---------------------------------------------------")
-print("brute force \t:", end=" ")
-print(a)
-print("time\t\t:", endb-startb, "second")
-print("---------------------------------------------------")
-print("div n con \t:", end=" ")
-print(b)
-print("time\t\t:", endd-startd, "second")
-print("---------------------------------------------------")
+    print("---------------------------------------------------")
+    print("brute force \t:", end=" ")
+    print(a)
+    print("time\t\t:", endb-startb, "second")
+    print("---------------------------------------------------")
+    print("div n con \t:", end=" ")
+    print(b)
+    print("time\t\t:", endd-startd, "second")
+    print("---------------------------------------------------")
+
+    vis = input("Mau nampilin visual? (y/n) ")
+    if(vis == 'y' or vis == 'Y'):
+        if(n>=1000):
+            vis = input("Yakin mau nampilin visual?(ngelag banget ntar) (y/n) ")
+            if(vis == 'y' or vis == 'Y'):
+                print("SIAPPPPP")
+                print("Resiko ditanggung sendiri")
+                showVisual(d,lll,titik1,titik2,b)
+        else:
+            showVisual(d,lll,titik1,titik2,b)
+    else:
+        print("TERIMAKASIH")
+    
+if __name__ == "__main__":
+    main()
